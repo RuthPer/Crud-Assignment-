f3a8f0bc40b38317927713efc34d6e77853943d9
Ruth Perjuste <RuthPer>
2025-11-11 18:45:34 -0500

    worked on proejct together

--- crud/app.py
+++ crud/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 import secrets
 import cs304dbi as dbi
+import db_methods
 
 # we need a secret_key to use flash() and sessions
 app.secret_key = secrets.token_hex()
@@ -15,7 +16,7 @@ app.secret_key = secrets.token_hex()
 # For CRUD and Ajax, use your personal db
 # For project work, use your team db
 
-print(dbi.conf('ww123_db'))
+print(dbi.conf('rp104_db'))
 
 # This gets us better error messages for certain common request errors
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
@@ -28,6 +29,99 @@ def index():
 def about():
     flash('this is a flashed message')
     return render_template('about.html', page_title='About Us')
+
+@app.route('/insert/',methods=["GET","POST"])
+def insert_movie():
+    """
+    This function will insert a new movie into the database, I f it is not a new
+    movie then it will send the user to the update page
+    """
+    conn=dbi.connect()
+    staff_id=10051
+    if request.method =="POST":
+        tt=request.form.get("movie_tt")
+        title=request.form.get("movie_title")
+        release=request.form.get("movie_release")
+        #Displays data that is missing from input
+        data={"Movies TT":tt,"Title":title,"Release":release}
+        for item in data:
+            if data[item]=="":
+                flash(f"Missing Input: {item}")
+        if "" not in [tt,title,release]:
+            result=db_methods.insert_movie(conn,int(tt),title,release,staff_id)
+            print(result)
+
+            if result==False:
+                return redirect(url_for("update_movie",tt=tt))
+
+
+    return render_template("form.html",page_title="Insert Movie")
+
+@app.route("/update_movie/<tt>",methods=["GET","POST"])
+def update_movie(tt):
+    """
+    This method allows the users to see update movies values 
+    """
+    staff_id=10051
+    conn=dbi.connect()
+    movie_data=db_methods.get_movie(conn,tt)
+    director=db_methods.get_director(conn,tt)
+    print(director)
+    if director==None:
+        director="None Specified"
+    else:
+        director=director['name']
+
+    if request.method =="POST":
+        title=request.form.get("title")
+        movie_id=request.form.get("movie_id")
+        release=request.form.get("release")
+        addedby=staff_id #Try to fix 
+        director_id=request.form.get("director_id")
+
+        option= request.form.get("select_type")
+        if option=="Delete":
+            db_methods.delete_movie(conn,tt)
+            flash(f"Movie: {title} was deleted successfully!")
+            return redirect(url_for("index"))
+        else:
+            db_methods.update_movie(conn,title,movie_id,release,addedby,director_id)
+            flash(f"Updated Movie: {title}")
+        
+
+
+    return render_template("update.html",page_title="Update Page",
+                        movie=movie_data,
+                        direct=director)
+
+@app.route("/select/",methods=["GET","POST"])
+def select_movie():
+    """
+    Selects all the movies in the database that are missing information in an column
+    that are either missing the director or release date
+    """
+    conn=dbi.connect()
+    movie_data=db_methods.select_movies(conn)
+    print(movie_data)
+    if movie_data==[]:
+        flash("There are no current incomplete Movies found!")
+
+    if request.method =="POST" and request.form.get("movie_selected")== "":
+        flash("Please select a movie ")
+    elif request.method =="POST":
+        tt=request.form.get("movie_selected")
+        return redirect(url_for('update_movie',tt=tt))
+
+    return render_template("select_movie.html",page_title="Select Incomplete Movies",
+                            movies=movie_data)
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     import sys, os
---
+++ crud/db_methods.py
@@ -0,0 +1,88 @@
+#By Ruth and Aayah
+import cs304dbi as dbi
+from flask import flash
+
+def insert_movie(conn,tt,title,release,addedby):
+    """
+    This will insert movies into the into the database
+    """
+    curs=dbi.dict_cursor(conn)
+
+    curs.execute("""
+                select count(*) from movie where tt=%s
+                """,[tt])
+
+    found_items=curs.fetchone()
+    print(found_items)
+
+    if found_items['count(*)']==0:
+        curs.execute("""
+                    insert into movie(tt,title,`release`,addedby) values(%s,%s,%s,%s)
+                    """,[tt,title,release,addedby])
+        conn.commit()
+        
+    else:
+        return False
+
+def get_movie(conn,tt):
+    """
+    Select all the information currently in the database on that certain movie and 
+    update element based on movie tt
+    """     
+    curs=dbi.dict_cursor(conn)
+
+    curs.execute("""
+                select * from movie where tt=%s
+                """,[tt])
+    return curs.fetchone()
+
+def get_director(conn,tt):
+    """
+    Select all the information currently in the database on the director for a movie 
+    based on movie id
+    """     
+    curs=dbi.dict_cursor(conn)
+
+    curs.execute("""
+                select * from movie m join person p on p.nm=m.director where tt=%s;
+                """,[tt])
+    return curs.fetchone()
+
+def delete_movie(conn,tt):
+    """
+    Will delete a specified movie form the database
+    """
+    curs=dbi.dict_cursor(conn)
+    curs.execute("""
+                    delete from movie where tt=%s
+                    """,[tt])
+    conn.commit()
+
+def update_movie(conn,title,tt,release,addedby,director):
+    """
+    Will update a specified movie form the database
+    """
+    curs=dbi.dict_cursor(conn)
+    curs.execute("""
+                    update movie set title=%s,tt=%s,`release`=%s,addedby=%s,director=%s where tt=%s
+                    """,[title,tt,release,addedby,director,tt])
+    conn.commit()
+
+def select_movies(conn):
+    """
+    Selects all the movies in the database that are missing information in an column
+    that are either missing the director or release date
+    """
+    curs=dbi.dict_cursor(conn)
+    curs.execute("""
+                select distinct* from movie where `release` is Null or director is Null 
+                """)
+    return curs.fetchall()
+
+
+if __name__ == '__main__':
+    dbi.conf("rp104_db")
+    conn=dbi.connect()
+    test=insert_movie(conn,213,"Little Women",1994)
+    
+    
